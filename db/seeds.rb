618c60c2ef3b0d4698edd7abc654e73fc39ee935
kouta-wolf <koutaookami18@gmail.com>
2026-06-29 13:40:57 +0900

    test: add テストユーザー（たくさんキャラ持ちと空のキャラ）2人追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+if Rails.env.development?
+  # 空状態の確認用ユーザーデータ
+  User.find_or_create_by!(email: "blank@test.com") do |u|
+    u.password = "password"
+  end
+
+  # 大量データの確認用ユーザーデータ
+  full_user = User.find_or_create_by!(email: "full@test.com") do |u|
+    u.password = "password"
+  end
+
+  if full_user.characters.count < 30
+    30.times do
+      full_user.characters.create!(
+        name:         Faker::Games::DnD.name,
+        dexterity:    rand(6..18),
+        agility:      rand(6..18),
+        strength:     rand(6..18),
+        vitality:     rand(6..18),
+        intelligence: rand(6..18),
+        spirit:       rand(6..18)
+      )
+    end
+  end
+end
