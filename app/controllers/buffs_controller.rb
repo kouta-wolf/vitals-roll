99809a7f9d2ec541042f2cc7199e4cf3150fa3d8
kouta-wolf <koutaookami18@gmail.com>
2026-07-14 16:35:33 +0900

    feat: プリセットを使わずバフを手動登録する機能を追加 (#30)
    バフ一覧をTurbo Frameでラップし、プリセットからの登録と手動登録を
    同じ枠内でインライン切り替えできるようにした。

--- app/controllers/buffs_controller.rb
+++ app/controllers/buffs_controller.rb
@@ -1,7 +1,22 @@
 class BuffsController < ApplicationController
   before_action :set_character
 
+  def new
+    @buff = @character.buffs.new
+    @buff_presets = BuffPreset.all
+  end
+
   def create
+    if buff_params.key?(:buff_preset_id)
+      create_from_preset
+    else
+      create_manual
+    end
+  end
+
+  private
+
+  def create_from_preset
     buff_preset = BuffPreset.find(buff_params[:buff_preset_id])
     @buff = buff_preset.build_buff_for(@character)
 
@@ -15,13 +30,28 @@ class BuffsController < ApplicationController
     end
   end
 
-  private
+  def create_manual
+    @buff = @character.buffs.new(manual_buff_params)
+    @buff.active = true
+    @buff.remaining_rounds = @buff.duration_rounds
+
+    if @buff.save
+      redirect_to character_path(@character), notice: "バフを作成しました"
+    else
+      @buff_presets = BuffPreset.all
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def set_character
     @character = current_user.characters.find(params[:character_id])
   end
 
   def buff_params
-    params.require(:buff).permit(:buff_preset_id)
+    params.require(:buff).permit(:buff_preset_id, :name, :target_status, :bonus_value, :duration_rounds)
+  end
+
+  def manual_buff_params
+    buff_params.except(:buff_preset_id)
   end
 end
---
+++ app/helpers/buffs_helper.rb
@@ -0,0 +1,17 @@
+module BuffsHelper
+  TARGET_STATUS_LABELS = {
+    "dexterity" => "器用度（DEX）",
+    "agility" => "敏捷度（AGI）",
+    "strength" => "筋力（STR）",
+    "vitality" => "生命力（VIT）",
+    "intelligence" => "知力（INT）",
+    "spirit" => "精神力（MND）",
+    "magic_power" => "魔法力",
+    "life_resistance" => "生命抵抗力",
+    "spirit_resistance" => "精神抵抗力"
+  }.freeze
+
+  def target_status_options
+    Buff::TARGET_STATUSES.map { |status| [ TARGET_STATUS_LABELS.fetch(status), status ] }
+  end
+end
