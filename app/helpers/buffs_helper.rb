5fe1b3a3151f6b4876e66a759280b78d036dbd28
kouta-wolf <koutaookami18@gmail.com>
2026-07-27 09:13:55 +0900

    feat: 判定式にダメージへ直接増減するバフを組み込めるようtarget_statusへdamageを追加 (#36)

--- app/helpers/buffs_helper.rb
+++ app/helpers/buffs_helper.rb
@@ -8,7 +8,8 @@ module BuffsHelper
     "spirit" => "精神力（MND）",
     "magic_power" => "魔法力",
     "life_resistance" => "生命抵抗力",
-    "spirit_resistance" => "精神抵抗力"
+    "spirit_resistance" => "精神抵抗力",
+    "damage" => "ダメージ"
   }.freeze
 
   def target_status_options
