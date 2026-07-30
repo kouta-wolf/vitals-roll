dd1ab0b508ef00eec9bffd4f57b197b94244e47d
kouta-wolf <koutaookami18@gmail.com>
2026-07-30 09:29:28 +0900

    test: ログイン済みアクセス時のリダイレクトのUIを先に固定するためrequest specを追加 (#86)

--- spec/requests/top_spec.rb
+++ spec/requests/top_spec.rb
@@ -2,9 +2,22 @@ require 'rails_helper'
 
 RSpec.describe "Top", type: :request do
   describe "GET /" do
-    it "正常にアクセスできるか" do
-      get root_path
-      expect(response).to have_http_status(200)
+    context "未ログインの場合" do
+      it "正常にアクセスできるか" do
+        get root_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "ログイン済の場合" do
+      let(:user) { create(:user) }
+
+      before { sign_in user }
+
+      it "characters_pathへリダイレクトされる" do
+        get root_path
+        expect(response).to redirect_to(characters_path)
+      end
     end
   end
 end
